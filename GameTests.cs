ed78ae94d29694768d99bc62f81747d1f1a533c7
A.A.Sapegin <sapegin04@mail.ru>
2022-10-25 11:53:17 +0500

    Add test OneCell

--- GameTests.cs
+++ GameTests.cs
@@ -1,14 +1,26 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 
 namespace GameOfLife
 {
     [TestFixture]
     public class GameTests
     {
-        [Test]
-        public void SomeTest()
+        private void Test(bool[,] input, bool[,] expectedResult)
         {
-            Assert.Fail();
+            var actualResult = Game.NextStep(input);
+            Assert.AreEqual(expectedResult.GetLength(0), actualResult.GetLength(0));
+            Assert.AreEqual(expectedResult.GetLength(1), actualResult.GetLength(1));
+            for (var i = 0; i < expectedResult.GetLength(0); i++) for (var j = 0; j < expectedResult.GetLength(1); j++)
+                Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void OneCellTest()
+        {
+            var input = new bool[,]{{false, false, false}, {false, true, false}, {false, false, false}};
+            var expectedResult = new bool[,] {{false, false, false}, {false, false, false}, {false, false, false}};
+            Test(input, expectedResult);
         }
     }
 }
